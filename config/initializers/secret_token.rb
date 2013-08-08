c8db1768593cb74bb79857a7d603b8a660ec5ea6
John Howlett <johnhowlett@new-host.home>
2013-08-08 17:29:57 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RailstutApp::Application.config.secret_key_base = 'acc14f863c91f44149f352e55469294feb403e99982c503c2935156bb1064d475c622f9398697351e0dbf5d2cb17b46a8bcd8225598412ca8749b396d590f73e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+RailstutApp::Application.config.session_store :cookie_store, key: '_railstut_app_session'
