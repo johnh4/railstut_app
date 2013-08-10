efcf9dfcc419355260a30197272de3843aa7cde1
John Howlett <johnhowlett@new-host.home>
2013-08-10 14:09:42 -0400

    Finish chapter 5

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,15 @@
 RailstutApp::Application.routes.draw do
+  get "users/new"
   get "static_pages/home"
   get "static_pages/help"
   get "static_pages/about"
+
+  get "/signup", to: "users#new", as: "/signup"
+  get "/contact", to: "static_pages#contact", as: "/contact"
+  get "/help", to: "static_pages#help", as: "/help"
+  get "/about", to: "static_pages#about", as: "/about"
+
+  root "static_pages#home"
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
