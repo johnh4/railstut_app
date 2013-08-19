55dc3ec298c45a5a1380d1864b1466d14c79f8f0
johnh4 <jehowl4@gmail.com>
2013-08-19 19:35:43 -0400

    Finish chapter 10

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -25,6 +25,7 @@ class UsersController < ApplicationController
 
   def show
   	@user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(page: params[:page])
   end
 
   def edit
@@ -51,14 +52,7 @@ class UsersController < ApplicationController
 
   	def user_params
   		params.require(:user).permit(:name, :email, :password, :password_confirmation)
-  	end
-
-    def signed_in_user
-      unless signed_in?
-        store_location
-        redirect_to signin_path, notice: "Please sign in."
-      end
-    end    
+  	end    
 
     def correct_user
       @user = User.find(params[:id])
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -29,6 +29,13 @@ module SessionsHelper
 		current_user == user
 	end
 
+    def signed_in_user
+      unless signed_in?
+        store_location
+        redirect_to signin_path, notice: "Please sign in."
+      end
+    end
+
 	def store_location
 		session[:return_to] = request.url
 	end
